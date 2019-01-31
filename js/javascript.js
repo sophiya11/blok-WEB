db3cc6f0b7a23c8baf188a749a088e7ff6a2d0d8
Sophiya Thangarajah <Sophiya@033.189.028.145.hva.nl>
2019-01-31 11:17:02 +0100

    Beoordeling

--- js/javascript.js
+++ js/javascript.js
@@ -2,4 +2,21 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
+/*microinteractie uitklappen filtermenu overlay*/
 
+function openNav() {
+  document.getElementById("myFiltermenu").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("myFiltermenu").style.width = "0";
+}
+
+
+/*microinteractie toevoegen aan leeslijst
+
+$(".more").click(function() {
+  $(this).toggleClass("toggle");
+});
+
+*/
